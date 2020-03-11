d258fe038586576f0f7aedd3bab219e5e33bc2df
Z. Patrick Lewis <patrick_lewis@unc.edu>
2020-03-11 06:48:45 +0000

    about us html validation fixes

--- resources/views/subheader.blade.php
+++ resources/views/subheader.blade.php
@@ -1,8 +1,8 @@
-<section class="subheader" style="background-image: url({{ asset('/img/' . $subheaderBg) }});">
+<div class="subheader" style="background-image: url({{ asset('/img/' . $subheaderBg) }});">
     <div class="jumbotron jumbotron-fluid">
         <div class="container text-center">
             <span class="text-center">{{ isset($subtext) ? $subtext : 'Ask Questions, Request Rides' }}</span>
-            <h1 class="text-uppercase text-center text-white">{{ $subheaderText }}</h1>
+            <p class="text-uppercase text-center text-white subheader-title">{{ $subheaderText }}</p>
         </div> <!-- /container -->
     </div> <!-- /.jumbotron-fluid -->
-</section>
+</div>
